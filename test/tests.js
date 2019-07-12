6969829c2857d3d751229b6039f57d380dc685d5
chrispiccaro <chrispiccaro18@gmail.com>
2019-07-11 17:38:01 -0700

    changed spacing

--- test/tests.js
+++ test/tests.js
@@ -1,20 +1,20 @@
 const test = QUnit.test;
 
 function isEven(number) {
-    if(number % 2 === 0) return true;
-    return false;
+  if(number % 2 === 0) return true;
+  return false;
 }
 
 test ('given an even number, return true', function(assert) {
-    const inputNumber = 16;
-    const result = isEven(inputNumber);
-    const expected = true;
-    assert.equal(result, expected);
+  const inputNumber = 16;
+  const result = isEven(inputNumber);
+  const expected = true;
+  assert.equal(result, expected);
 });
 
 test ('given an odd number, return false', function(assert) {
-    const inputNumber = 13;
-    const result = isEven(inputNumber);
-    const expected = false;
-    assert.equal(result, expected);
+  const inputNumber = 13;
+  const result = isEven(inputNumber);
+  const expected = false;
+  assert.equal(result, expected);
 });
