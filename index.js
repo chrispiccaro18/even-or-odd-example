9a0334101f653916478191ba3a565e1fba2589fd
chrispiccaro <chrispiccaro18@gmail.com>
2019-07-11 17:06:35 -0700

    refactored

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ const numberInput = document.getElementById('number-input');
 const resultNode = document.getElementById('result');
 const submitButton = document.getElementById('submit-button');
 
-if(isEven(numberInput.value)) {
-  resultNode.textContent = 'even';
-} else {
-  resultNode.textContent = 'odd';
-}
+displayResult();
 
 submitButton.addEventListener('click', () => {
-  if(isEven(numberInput.value)) {
+  displayResult();
+});
+
+function displayResult() {
+  if (isEven(numberInput.value)) {
     resultNode.textContent = 'even';
-  } else {
+  }
+  else {
     resultNode.textContent = 'odd';
   }
-});
+}
 
 function isEven(number) {
   if(number % 2 === 0) return true;
